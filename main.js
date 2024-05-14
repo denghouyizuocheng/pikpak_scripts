395c717a8bb8de4d927ba4098218c18c8b3a2723
denghouyizuocheng <40046449+denghouyizuocheng@users.noreply.github.com>
2024-05-14 11:26:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ async function main() {
   const password = response.data.password;
 
   console.log(username, password);
-  let pikPak = new PikPak(username, password, null, "83718809");
+  let pikPak = new PikPak(username, password, null, "22760641");
 
   pikPak
     .authVerification()
